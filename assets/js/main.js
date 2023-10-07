524437db7a72e480f87606cfc06ca4ef8cf0cf0c
LeonWebSolution <leonwebdevv@gmail.com>
2023-10-07 18:49:12 +0300

    tablet  main

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,34 @@ closeModalButton.addEventListener("click", function () {
     modal.style.display = "none";
     body.style.overflow = "auto";
 });
+
+var swiper = new Swiper('.new-season__slider', {
+    slidesPerView: 4, 
+    spaceBetween: 30,
+    breakpoints: {
+        1025: {
+            slidesPerView: 4, 
+            spaceBetween: 20,
+        },
+        500: {
+            slidesPerView: 2, 
+            spaceBetween: 20,
+            loop: true,
+          },
+          320: {
+            slidesPerView: 'auto', 
+            spaceBetween: 20,
+            loop: false,
+          },
+      },
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerButton = document.querySelector('.hamburger');
+    const headerMenu = document.querySelector('.header-menu');
+  
+    hamburgerButton.addEventListener('click', function () {
+      headerMenu.classList.toggle('active'); 
+      hamburgerButton.classList.toggle('active');
+    });
+  });
